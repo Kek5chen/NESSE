ded99afdf6cd95b38cd33a988f044df8d476d802
Kek5chen <kx@imkx.dev>
2023-09-06 17:02:53 +0200

    added temporary PRG mirroring

--- Source/System/NES/ROMLoader/NES/NESLoader.cpp
+++ Source/System/NES/ROMLoader/NES/NESLoader.cpp
@@ -10,11 +10,16 @@ bool NESLoader::loadROM(const std::filesystem::path &romPath, NES *nes) {
 		WLOG("Could not write PRGROM to Memory");
 		return false;
 	}
+	if (!nes->Bus.mMemory.write(0xBFFF, rom.Data.PRGROM.data(), rom.Data.PRGROM.size())) {
+		WLOG("Could not write PRGROM to Memory");
+		return false;
+	} // TODO: MAPPER!!!!!!
 	if (!nes->Bus.mPPU.write(0, rom.Data.CHRROM.data(), rom.Data.CHRROM.size())) {
 		WLOG("Could not write CHRROM to PPU Memory");
 		return false;
 	}
 	ILOG("Successfully loaded ROM.");
 	// TODO: Implement mappers
+
 	return true;
 }
